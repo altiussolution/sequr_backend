4039b78b2981e27356a5f96f137b8c6d09994a7b
Priyadharshini <priyadharshini.kathirvel@adcltech.com>
2021-09-06 12:23:57 +0530

    branch_api

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,8 +4,9 @@ let Category = require('./category.route')
 let Item = require('./item.route')
 let Region = require('./region.route')
 let Shift = require('./shift.route')
-Departments = require('./department.route'),
-    Branch = require('./branch.route')
+let Departments = require('./department.route')
+let Branch = require('./branch.route')
+let Cube = require('./cube.route')
 
 module.exports = (app) => {
     app.use('/users', Users)
@@ -16,6 +17,7 @@ module.exports = (app) => {
     app.use('/shift', Shift)
     app.use('/department', Departments)
     app.use('/branch', Branch)
+    app.use('/cube', Cube)
 }
 
 
