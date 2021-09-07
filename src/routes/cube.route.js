e99e6ce5a901f414c276bf8b05c5909490559e46
Priyadharshini <priyadharshini.kathirvel@adcltech.com>
2021-09-07 10:06:23 +0530

    cube_api

--- src/routes/cube.route.js
+++ src/routes/cube.route.js
@@ -2,7 +2,9 @@ const { CubeController } = require('../controllers')
 
 let route = require('express').Router()
 
-
+route.post('/add', CubeController.createCube);
+route.get('/get', CubeController.getCube);
+route.put('/update/:id', CubeController.updateCube);
 
 
 
