3c10994d2532b1aa7871119b07e78c3ba1452755
meenakshiva <meenakshi.shivamurthi@adcltech.com>
2021-09-07 14:51:05 +0530

    Error Fixed

--- src/routes/roles.route.js
+++ src/routes/roles.route.js
@@ -1,6 +1,7 @@
 let route = require('express').Router()
 var controllers = require('../controllers/index');
-auth, 
+const auth = require("../middleware/auth.middleware");
+
 route.post('/add', auth, controllers.RolesController.createRole)
 route.put('/permission/update/:id', auth, controllers.RolesController.updatePermission)
 route.get('/get', auth, controllers.RolesController.getRoles)
