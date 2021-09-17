42de5335cd8018e12b62e429b62c4c8ed187e16f
meenakshiva <meenakshi.shivamurthi@adcltech.com>
2021-09-17 13:02:29 +0530

    item route

--- src/routes/item.route.js
+++ src/routes/item.route.js
@@ -13,9 +13,9 @@ var storage = multer.diskStorage({
 var upload = multer({ storage: storage })
 
 route.post('/add', auth, ItemController.addItem)
-route.get('/get', ItemController.getItem)
+route.get('/get', auth, ItemController.getItem)
 route.put('/update/:id', auth, ItemController.updateItem)
-route.post('/upload', upload.array('image-video'), ItemController.upload)
+route.post('/upload', auth, upload.array('image-video'), ItemController.upload)
 route.get('/getItemByCategory/:category_id', auth, ItemController.getItemByCategory)
 
 module.exports = route
