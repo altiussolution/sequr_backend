bacf3e27626d6ca769ef8c135d14124acab4e6b0
prakashpalaniappan <prakash.palaniappan@adcltech.com>
2021-09-17 12:47:30 +0530

    purchaseOrder delete update

--- src/controllers/purchaseOrder.controller.js
+++ src/controllers/purchaseOrder.controller.js
@@ -1,5 +1,5 @@
 const { purchaseOrderModel } = require("../models");
-const {error_code} = require("../utils/enum.utils")
+const {error_code,appRouteModels} = require("../utils/enum.utils")
 
 exports.addPurchaseOrder = (async (req, res) => {
     try{
@@ -46,5 +46,41 @@ exports.getPurchaseOrder = (async (req, res) => {
     } catch (error) {
         res.send("An error occured");
         console.log(error);
+    }
+})
+exports.updatePurchaseOrder = (async (req, res) => {
+    console.log(req.params.id)
+    try{
+        purchase_orderModel.findByIdAndUpdate(req.params.id, req.body).then(purchase_orderUpdate =>{
+            res.status(200).send({ success: true, message: 'PurchaseOrder Updated Successfully!' });
+        }).catch(error =>{
+            res.status(200).send({ success: false, error: error, message : 'An Error Occured' });
+        }) 
+    }catch(err){
+        res.status(200).send({ success: false, error: err, message : 'An Error Catched' });  
+    }
+})
+exports.deletePurchaseOrder = (req, res) => {
+    try {
+        purchase_orderModel.findByIdAndUpdate(req.params.id, { active_status: 0 }).then(purchase_order => {
+            res.status(200).send({ success: true, message: 'PurchaseOrder Deleted Successfully!' });
+        }).catch(err => {
+            res.status(200).send({ success: false, message: 'PurchaseOrder Not Found' });
+        })
+    }
+    catch (err) {
+        res.status(200).send({ success: false, error: err, message: 'An Error Catched' });
+    }
+
+}
+exports.upload = (async(req,res) => {
+    try{
+      if(req.file){
+        var filename = req.file.originalname
+        res.status(200).send({message : 'PurchaseOrder Invoice Sucessfully', Path : `${req.file.destination.replace('./src/public/',appRouteModels.BASEURL)}/${filename}`})
+      }
+    }catch (err) {
+        console.log(err)
+      res.status(400).send(err);
     }
 })
