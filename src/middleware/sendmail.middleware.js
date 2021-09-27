5ff55a1e635beb1f079852ff316774b6e6deec69
meenakshiva <meenakshi.shivamurthi@adcltech.com>
2021-09-27 11:10:13 +0530

    Node Mailer changed

--- src/middleware/sendmail.middleware.js
+++ src/middleware/sendmail.middleware.js
@@ -3,10 +3,10 @@ const nodemailer = require("nodemailer");
 const sendEmail = async (email, subject, text) => {
     try {
         const transporter = nodemailer.createTransport({
-            host: process.env.HOST,
+            // host: process.env.HOST,
             service: process.env.SERVICE,
-            port: 587,
-            secure: true,
+            // port: 587,
+            // secure: true,
             auth: {
                 user: process.env.USER,
                 pass: process.env.PASS,
