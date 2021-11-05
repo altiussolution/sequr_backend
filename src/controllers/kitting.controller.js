5b803db944de6207ece1defdebb9bce260c86850
meenakshiva <meenakshi.shivamurthi@adcltech.com>
2021-11-05 13:19:15 +0530

    kitting updated

--- src/controllers/kitting.controller.js
+++ src/controllers/kitting.controller.js
@@ -66,12 +66,13 @@ exports.getKit = (req, res) => {
               .populate('bin', ['bin_name', 'bin_id'])
               .populate('compartment', ['compartment_name', 'compartment_id'])
               .exec()
-              allocationDetais['kit_item_description'] = kitdata['description']
-              allocationDetais['kit_item_qty'] = kitdata['qty']
-              allocationDetais['kit_item_pack_id'] = kitdata['_id']
-              allocationDetais['kit_item_id'] = kitdata['kitData.item_id']
+              kitItemAllocation = JSON.parse(JSON.stringify(allocationDetais))
+              kitItemAllocation['kit_item_description'] = kitdata['description']
+              kitItemAllocation['kit_item_qty'] = kitdata['qty']
+              kitItemAllocation['kit_item_pack_id'] = kitdata['_id']
+              kitItemAllocation['kit_item_id'] = kitdata['kitData.item_id']
             //   itemDetails['itemDetails'] = allocationDetais
-            await kitData.push(allocationDetais)
+            await kitData.push(kitItemAllocation)
             index++
           }
           await binDatas.push({
