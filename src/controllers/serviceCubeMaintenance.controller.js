0b1d179a1a14fe7a7ac2362c15431c3d5b6db9a6
meenakshiva <meenakshi.shivamurthi@adcltech.com>
2021-11-09 17:29:12 +0530

    Cube Idle Hours Updated

--- src/controllers/serviceCubeMaintenance.controller.js
+++ src/controllers/serviceCubeMaintenance.controller.js
@@ -8,6 +8,7 @@ exports.cubeIdleHours = async (req, res) => {
       .find({
         active_status: 1
       })
+      .populate('branch_id')
       .exec()
 
     oveallmachineUsage = []
@@ -60,10 +61,10 @@ exports.cubeIdleHours = async (req, res) => {
             }
           ])
           .exec()
-          console.log(cubeUsage)
         eachCubeUsage['date'] = dateForUsage
         eachCubeUsage['cube'] = cube.cube_id
         eachCubeUsage['cube_name'] = cube.cube_name
+        eachCubeUsage['branch'] = cube.branch_id
         if (cubeUsage[0]) {
           eachCubeUsage['cube_usage'] = cubeUsage[0].sum
           eachCubeUsage['cube_idle'] = 8.64e7 - cubeUsage[0].sum
@@ -90,59 +91,62 @@ exports.filterCubeIdleHours = async (req, res) => {
   startDay = moment(body.date).format('YYYY-MM-DD 00:00:01')
   endDay = moment(body.date).format('YYYY-MM-DD 23:59:59')
 
-  //   try {
-  cubes = await cubeModel
-    .find({
-      active_status: 1,
-      branch_id : body.branch_id
-    })
-    .exec()
-
-  oveallmachineUsage = []
-
-  for await (let cube of cubes) {
-    let eachCubeUsage = {}
-    cubeUsage = await machineUsageModel
-      .aggregate([
-        {
-          $match: {
-            $and: [
-              { cube_id: cube._id },
-              { created_at: { $gt: new Date(startDay) } },
-              {
-                created_at: {
-                  $lt: new Date(endDay)
-                }
-              }
-            ]
-          }
-        },
-        { $group: { _id: null, sum: { $sum: '$machine_usage' } } }
-      ])
+  try {
+    cubes = await cubeModel
+      .find({
+        active_status: 1,
+        branch_id: body.branch_id
+      })
+      .populate('branch_id')
       .exec()
-    eachCubeUsage['date'] = startDay
-    eachCubeUsage['cube_id'] = cube.cube_id
-    if (cubeUsage[0]) {
-      eachCubeUsage['cube_usage'] = cubeUsage[0].sum
-      eachCubeUsage['cube_idle'] = 8.64e7 - cubeUsage[0].sum
-      8.64e7
-    } else {
-      eachCubeUsage['cube_usage'] = 0
-      eachCubeUsage['cube_idle'] = 8.64e7
-    }
-    await oveallmachineUsage.push(eachCubeUsage)
-  }
 
-  res.status(200).send({ success: true, data: oveallmachineUsage })
-  //   } catch (error) {
-  //     res.status(201).send({ success: false, error: error })
-  //   }
+    oveallmachineUsage = []
+
+    for await (let cube of cubes) {
+      let eachCubeUsage = {}
+      cubeUsage = await machineUsageModel
+        .aggregate([
+          {
+            $match: {
+              $and: [
+                { cube_id: cube._id },
+                { created_at: { $gt: new Date(startDay) } },
+                {
+                  created_at: {
+                    $lt: new Date(endDay)
+                  }
+                }
+              ]
+            }
+          },
+          { $group: { _id: null, sum: { $sum: '$machine_usage' } } }
+        ])
+        .exec()
+      eachCubeUsage['date'] = startDay
+      eachCubeUsage['cube_id'] = cube.cube_id
+      eachCubeUsage['cube_name'] = cube.cube_name
+      eachCubeUsage['branch'] = cube.branch_id
+
+      if (cubeUsage[0]) {
+        eachCubeUsage['cube_usage'] = cubeUsage[0].sum
+        eachCubeUsage['cube_idle'] = 8.64e7 - cubeUsage[0].sum
+        8.64e7
+      } else {
+        eachCubeUsage['cube_usage'] = 0
+        eachCubeUsage['cube_idle'] = 8.64e7
+      }
+      await oveallmachineUsage.push(eachCubeUsage)
+    }
+
+    res.status(200).send({ success: true, data: oveallmachineUsage })
+  } catch (error) {
+    res.status(201).send({ success: false, error: error })
+  }
 }
 
 async function calculatDate (subtractDay) {
   var d = new Date()
   await d.setDate(d.getDate() - subtractDay)
   var date = await d.toISOString().slice(0, 10)
-  console.log(date)
   return date
 }
