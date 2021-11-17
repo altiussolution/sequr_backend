2286c92d868f4dc6fe0e0cebb52e4d6d30fd4b87
prakashpalaniappan <prakash.palaniappan@adcltech.com>
2021-11-17 10:58:23 +0530

    cubedelete

--- src/routes/cube.route.js
+++ src/routes/cube.route.js
@@ -6,6 +6,7 @@ route.post('/add', auth,CubeController.createCube);
 route.get('/get', auth,CubeController.getCube);
 route.put('/update/:id', auth,CubeController.updateCube);
 route.get('/getCubefilter', auth,CubeController.getCubefilter);
+route.delete('/delete/:id', auth,CubeController.deleteCube);
 
 
 
