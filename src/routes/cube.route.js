5116870d03e2452c84f2b19b153f67ae245ad2c8
Gokulkrishna3 <gokul.krishna@adcltech.com>
2021-11-17 18:19:29 +0530

    dall

--- src/routes/cube.route.js
+++ src/routes/cube.route.js
@@ -6,11 +6,7 @@ route.post('/add', auth,CubeController.createCube);
 route.get('/get', auth,CubeController.getCube);
 route.put('/update/:id', auth,CubeController.updateCube);
 route.get('/getCubefilter', auth,CubeController.getCubefilter);
-<<<<<<< HEAD
 route.delete('/delete/:id',auth,CubeController.deleteCube);
-=======
-route.delete('/delete/:id', auth,CubeController.deleteCube);
->>>>>>> 16998c59390e2f4fd3952bde6cac886dadc43c69
 
 
 
