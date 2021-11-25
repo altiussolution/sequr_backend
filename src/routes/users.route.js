5b08ee9f4f9b340b9278ef14ef12dd96565cf61d
meenakshiva <meenakshi.shivamurthi@adcltech.com>
2021-11-25 21:50:13 +0530

    users updated

--- src/routes/users.route.js
+++ src/routes/users.route.js
@@ -10,19 +10,28 @@ var multer = require("multer");
 //       cb(null, `${req.user.user_id}.${file.originalname.split('.').pop()}`)
 //     }
 // })
+// const storage = multer.diskStorage({
+//   destination: function (req, files, cb) {
+//     cb(null, './src/public/uploads')
+//   },
+//   filename: function (req, files, cb) {
+//     cb(null, `${new Date().toString()}_${files.originalname}`)
+//   }
+// })
 const storage = multer.diskStorage({
-  destination: function (req, files, cb) {
+  destination: function (req, file, cb) {
     cb(null, './src/public/uploads')
   },
-  filename: function (req, files, cb) {
-    cb(null, files.originalname)
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+    cb(null, uniqueSuffix +'_'+ file.originalname)
   }
 })
 var upload = multer({ storage: storage })
 
 route.post('/add', controllers.UsersController.add)
 route.post('/login', controllers.UsersController.login)
-route.post('/upload',auth, upload.single('profile'),controllers.UsersController.upload)
+route.post('/upload', upload.single('profile'),controllers.UsersController.upload)
 route.put('/update/:_id', auth, controllers.UsersController.update)
 route.put('/delete', auth, controllers.UsersController.delete)
 route.get('/get', controllers.UsersController.listEmployees)
