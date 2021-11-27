13b722e50aa86e719fbef1ab1ced533cbb0bfa66
meenakshiva <meenakshi.shivamurthi@adcltech.com>
2021-11-27 10:49:05 +0530

    new add api for all

--- src/models/department.model.js
+++ src/models/department.model.js
@@ -39,15 +39,9 @@ const DepartmentSchema = Schema(
   { timestamps: { updatedAt: 'updated_at' } }
 )
 
-<<<<<<< HEAD
-DepartmentSchema.index({ department_name: 1, company_id: 1 }, { unique: true })
-DepartmentSchema.index({ department_id: 1, company_id: 1 }, { unique: true })
-module.exports = mongoose.model('department', DepartmentSchema)
-=======
 // DepartmentSchema.index( { "department_name": 1, "department_id": 1,"company_id" : 1 }, { unique: true } )
 
 DepartmentSchema.index({ department_name: 1, company_id: 1 }, { unique: true })
 DepartmentSchema.index({ department_id: 1, company_id: 1 }, { unique: true })
 
 module.exports = mongoose.model('department', DepartmentSchema);
->>>>>>> 5bef60a76491367ceb6846f5683975eafe660b7f
