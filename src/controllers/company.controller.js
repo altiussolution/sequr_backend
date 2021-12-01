61896ea023a33a6962e436d89e4825fd44488c62
meenakshiva <meenakshi.shivamurthi@adcltech.com>
2021-12-01 16:08:22 +0530

    permission added

--- src/controllers/company.controller.js
+++ src/controllers/company.controller.js
@@ -44,10 +44,10 @@ exports.createCompany = (req, res) => {
             .status(422)
             .send({
               success: false,
-              message: tiltelCase(`${Object.keys(err.keyPattern)[0].replace(
+              message: (tiltelCase(`${Object.keys(err.keyPattern)[0].replace(
                 '_',
                 ' '
-              )} already exist`)
+              )} already exist`)).replace('Compartment', 'Draw')
             }) // Paste your validation fields
         }
       }
--- src/models/supplier.model.js
+++ src/models/supplier.model.js
@@ -1,83 +1,89 @@
 var mongoose = require('mongoose'),
-    Schema = mongoose.Schema;
+  Schema = mongoose.Schema
 
-const SupplierSchema = Schema({
+const SupplierSchema = Schema(
+  {
     supplier_name: {
-        type: String,
-        required: true
+      type: String,
+      required: true
     },
     supplier_code: {
-        type: String,
-        required: true
+      type: String,
+      required: true
     },
     supplier_address: {
-        type: String,
-        required: true
+      type: String,
+      required: true
     },
     country_id: {
-        type: Schema.Types.ObjectId,
-        required: true,
-        ref: 'country'
+      type: Schema.Types.ObjectId,
+      required: true,
+      ref: 'country'
     },
     state_id: {
-        type: Schema.Types.ObjectId,
-        ref: 'state'
+      type: Schema.Types.ObjectId,
+      ref: 'state'
     },
     city_id: {
-        type: Schema.Types.ObjectId,
-        ref: 'city'
+      type: Schema.Types.ObjectId,
+      ref: 'city'
     },
-    
+
     zip_code: {
-        type: String,
-        required: true,
+      type: String,
+      required: true
     },
     phone_number: {
-        type: String,
-        required: true
+      type: String,
+      required: true
     },
     fax: {
-        type: String,
-        required: false
+      type: String,
+      required: false
     },
     email_id: {
-        type: String,
-        required: true
+      type: String,
+      required: true
     },
     po_email: {
-        type: String,
-        required: true,
+      type: String,
+      required: true
     },
     company_id: {
-        type: Schema.Types.ObjectId,
-        ref: 'company',
-        required : true
-      },
+      type: Schema.Types.ObjectId,
+      ref: 'company',
+      required: true
+    },
     status: {
-        type: Boolean,
-        default: false
+      type: Boolean,
+      default: false
     },
     created_at: {
-        type: Date,
-        default: Date.now
+      type: Date,
+      default: Date.now
     },
     deleted_at: {
-        type: Date,
-        default: null
+      type: Date,
+      default: null
     },
     updated_at: {
-        type: Date,
-        default: Date.now
+      type: Date,
+      default: Date.now
     },
     active_status: {
-        type: Number,
-        enum: [0,1],
-        default: 1
+      type: Number,
+      enum: [0, 1],
+      default: 1
     }
-},
-{ timestamps: { updatedAt: 'updated_at' } }
+  },
+  { timestamps: { updatedAt: 'updated_at' } }
 )
-SupplierSchema.index({'$**': 'text'});
+SupplierSchema.index({ '$**': 'text' })
 
-SupplierSchema.index({ "supplier_name":1,"supplier_code": 1, "supplier_address" : 1, "email_id":1, "phone_number":1, "company_id": 1 }, { unique: true });
-module.exports = mongoose.model('supplier', SupplierSchema);
+SupplierSchema.index({ supplier_name: 1, company_id: 1 }, { unique: true })
+SupplierSchema.index({ supplier_code: 1, company_id: 1 }, { unique: true })
+SupplierSchema.index({ supplier_address: 1, company_id: 1 }, { unique: true })
+SupplierSchema.index({ phone_number: 1, company_id: 1 }, { unique: true })
+SupplierSchema.index({ email_id: 1, company_id: 1 }, { unique: true })
+
+module.exports = mongoose.model('supplier', SupplierSchema)
