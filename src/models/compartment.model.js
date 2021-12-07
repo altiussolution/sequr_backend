29d7077d5539f3b94b4a1ec7f0822b4cffedf51b
meenakshiva <meenakshi.shivamurthi@adcltech.com>
2021-12-07 16:38:13 +0530

    Compartment model changed

--- src/models/compartment.model.js
+++ src/models/compartment.model.js
@@ -1,88 +1,95 @@
 var mongoose = require('mongoose'),
-    Schema = mongoose.Schema;
+  Schema = mongoose.Schema
 
-const CompartmentSchema = Schema({
+const CompartmentSchema = Schema(
+  {
     compartment_name: {
-        type: String,
-        required: true
+      type: String,
+      required: true
     },
     compartment_id: {
-        type: String,
-        required: true,
+      type: String,
+      required: true
     },
     cube_id: {
-        type: Schema.Types.ObjectId,
-        required: true,
-        ref: 'cube'
+      type: Schema.Types.ObjectId,
+      required: true,
+      ref: 'cube'
     },
     bin_id: {
-        type: Schema.Types.ObjectId,
-        required: true,
-        ref: 'bin'
+      type: Schema.Types.ObjectId,
+      required: true,
+      ref: 'bin'
     },
     auto_purchase_order: {
-        type: Boolean,
-        default: false
+      type: Boolean,
+      default: false
     },
     item_alert: {
-        type: Boolean,
-        default: false
+      type: Boolean,
+      default: false
     },
     print_receipt: {
-        type: Boolean,
-        default: false
+      type: Boolean,
+      default: false
     },
     alert_on: {
-        type: Number,
-        required: false
+      type: Number,
+      required: false
     },
     generate_po_on: {
-        type: Number,
-        required: false
+      type: Number,
+      required: false
     },
     item_max_cap: {
-        type: Number,
-        required: true
+      type: Number,
+      required: true
     },
     item_min_cap: {
-        type: Number,
-        required: true
+      type: Number,
+      required: true
     },
     description: {
-        type: String
+      type: String
     },
     is_removed: {
-        type: Boolean,
-        default: false
+      type: Boolean,
+      default: false
     },
     created_at: {
-        type: Date,
-        default: Date.now
+      type: Date,
+      default: Date.now
     },
     deleted_at: {
-        type: Date,
-        default: null
+      type: Date,
+      default: null
     },
     updated_at: {
-        type: Date,
-        default: Date.now
+      type: Date,
+      default: Date.now
     },
     company_id: {
-        type: Schema.Types.ObjectId,
-        ref: 'company',
-        required : true
-      },
+      type: Schema.Types.ObjectId,
+      ref: 'company',
+      required: true
+    },
     active_status: {
-        type: Number,
-        enum: [0,1],
-        default: 1
+      type: Number,
+      enum: [0, 1],
+      default: 1
     }
-},
-{ timestamps: { updatedAt: 'updated_at' } }
+  },
+  { timestamps: { updatedAt: 'updated_at' } }
 )
 //  CompartmentSchema.index({ "compartment_name": 1, "company_id": 1 }, { unique: true });
-CompartmentSchema.index({ compartment_name: 1, company_id: 1 }, { unique: true })
-CompartmentSchema.index({ compartment_id: 1, company_id: 1 }, { unique: true })
+CompartmentSchema.index(
+  { compartment_name: 1, company_id: 1 },
+  { unique: true }
+)
+CompartmentSchema.index(
+  { cube_id: 1, compartment_id: 1, bin_id: 1, company_id: 1 },
+  { unique: true }
+)
 
-CompartmentSchema.index({'$**': 'text'});
-module.exports = mongoose.model('compartment', CompartmentSchema);
+CompartmentSchema.index({ '$**': 'text' })
+module.exports = mongoose.model('compartment', CompartmentSchema)
