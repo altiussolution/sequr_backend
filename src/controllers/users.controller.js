c82c1814481ca7e12b24f7c2ec4715802c577900
prakashpalaniappan <prakash.palaniappan@adcltech.com>
2022-08-08 11:38:32 +0530

    sequrmodelstore

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -982,12 +982,12 @@ function kitdetailsadd () {
     var options = { upsert: true, new: true, setDefaultsOnInsert: true }
     //var cart_status = output.data.Kits[i].cart_status
     var qty = output.data.Kits[i].qty
-   var kitAdding = ({
-      kitting: kits,
-      kit_id: kit_id,
-      kit_status: kit_status,
-      //cart_status : cart_status,
-      item_quantity : qty
+   var kitAdding = AddKit({
+    kitData: output.data.Kits,
+          kit_id : kit_id,
+          kit_status : kit_status,
+          item_quantity : qty,
+          qty : 1
     })
    
      
---
+++ src/services/kit.services.js
@@ -0,0 +1,28 @@
+module.exports = function AddKit(kitData){ //oldCart,item,allocation,userQty
+	var oldKit = kitData.kitData;
+	var kit_id = kitData.kit_id;
+	var item_quantity = kitData.item_quantity;
+	var qty = kitData.qty;
+	//var updateQty = cartData.updateQty
+
+	this.items = oldKit.kit || [];
+	 if(this.items.length == 0){
+		this.items.push({kit_id : kit_id, item_quantity : item_quantity, qty : qty, kit_status : 1})
+	}else{
+		var checkIsItemmExist = this.items.filter(obj => (obj.item == item && obj.kit_status == 1));
+		
+		if(checkIsItemmExist.length == 0){
+			this.items.push({kit_id : kit_id, item_quantity : item_quantity, qty : qty, kit_status : 1})
+		}
+		else{
+			var index = this.items.findIndex(p => p.item == item && p.kit_status == 1);
+			this.items[index]['qty'] = updateQty ? userQty : this.items[index]['qty']+userQty
+		}
+	}
+	oldKit['kit'] = this.items
+	// data.total_quantity = data.cart.reduce((acc, curr) => acc + curr.qty, 0); // 6; current.cart_status == 1
+	oldKit.total_quantity = this.items.reduce(function(sum, current) {
+		return current.kit_status == 1 ? sum + current.qty : sum;
+	}, 0);
+	return oldKit;
+}
