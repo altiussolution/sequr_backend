2c2d072ed107068d1ea60a6419452c9287e89015
prakashpalaniappan <prakash.palaniappan@adcltech.com>
2022-08-09 18:31:23 +0530

    sequrmodelstore

--- src/services/kit.services.js
+++ src/services/kit.services.js
@@ -14,10 +14,10 @@ module.exports = function AddKit(kitData){ //oldCart,item,allocation,userQty
 		if(checkIsItemmExist.length == 0){
 			this.items.push({kit_id : kit_id, item_quantity : item_quantity, qty : qty, kit_status : 1})
 		}
-		else{
-			var index = this.items.findIndex(p => p.kit_id == kit_id && p.kit_status == 1);
-			this.items[index]['qty'] = updateQty ? userQty : this.items[index]['qty']+userQty
-		}
+		// else{
+		// 	var index = this.items.findIndex(p => p.kit_id == kit_id && p.kit_status == 1);
+		// 	this.items[index]['qty'] = updateQty ? userQty : this.items[index]['qty']+userQty
+		// }
 	}
 	oldKit['kit'] = this.items
 	// data.total_quantity = data.cart.reduce((acc, curr) => acc + curr.qty, 0); // 6; current.cart_status == 1
