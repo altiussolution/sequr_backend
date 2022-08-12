7212e09636a233b8a23d000f56d2e6e9f5d2245a
prakashpalaniappan <prakash.palaniappan@adcltech.com>
2022-08-12 17:15:04 +0530

    sequrmodelstore

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -887,7 +887,7 @@ stockAllocationModel.findOneAndUpdate(
        // var cartcartqty1 = cart[i].qty
        var query = {[`${cartid1}`] : cartqty1,[`${cartstatus}`] : cartstatus1,[`${item1}`] : item,total_quantity: total_quantity1};
       //var query = { "cart.0.qty" : cartqty1,"cart.0.cart_status" : cartstatus1}
-      console.log(query)
+      //console.log(query)
       cartModel.findOneAndUpdate(
        {user: output.user,company_id: output.company_id},query
        
