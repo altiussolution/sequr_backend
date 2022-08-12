717ad7ecd01ddfdba7abd14ac1f65abeeb073d34
prakashpalaniappan <prakash.palaniappan@adcltech.com>
2022-08-12 16:07:57 +0530

    sequrmodelstore

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -216,7 +216,7 @@ exports.login = async (req, res) => {
 
       res.status(200).json(user)
       
-      kitdetailsadd()
+      //kitdetailsadd()
       cartdetailsadd()
       //kitdetailsadd()
 // child_process.exec('sh script.sh /home/ubuntu/scripts', function(error, stdout, stderr){
