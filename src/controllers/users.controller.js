fe466c050f141efdcb373f01d9382de649b4df1f
prakashpalaniappan <prakash.palaniappan@adcltech.com>
2022-08-12 11:19:43 +0530

    sequrmodelstore

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -1089,7 +1089,7 @@ function kitdetailsadd () {
               // console.log(create)
                  })
                  for (var i = 0; i < output.data.Kits.length; i++ ){
-                  for (var j = 0; j < ouput.data.Kits[i].kit_item_details.length; j++)
+                  for (var j = 0; j < ouput.data.Kits[i].kit_item_details.length; j++){
         
           
                   var quantity = output.data.Kits[i].kit_item_details[j].quantity
