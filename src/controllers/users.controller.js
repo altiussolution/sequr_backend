2ae4c504b14f44fc68a5823be87d97d5ff6dd5cf
prakashpalaniappan <prakash.palaniappan@adcltech.com>
2022-08-17 15:57:15 +0530

    sequrmodelstore

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -812,7 +812,7 @@ function cartdetails () {
     storeModel.findOne({cartinfo : 2}).sort({created_at : -1 } )
    .limit(1)
    .then (output => {
-    console.log(output)
+    //console.log(output)
      
     updateDate(output)
    
@@ -833,7 +833,7 @@ function cartdetails () {
           cartModel.findOne({}).sort({updated_at: -1}).limit(1).then(updated => {
             //cartModel.findOne({}).limit(1).sort({updated_at: -1}).then(updated => {
             var updated = updated.updated_at
-             //console.log(updated,"Date")
+             console.log(updated,"Date")
              if (output.created_at > updated){
               if (output.updatestatus == 1){
                 for (var i = 0; i < output.data.cart.length; i++ ){
